b6a8cacf608911fe770cd67925ced7b1a78f1b45
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-11-26 20:40:29 +0200

    delete useSelector

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getContactsThunk } from './contacts/thunk';
 import Header from './Header';
 import ContactList from './ContactList/ContactList';
